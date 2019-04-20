f73f6c11d967c3754cebaae0fbe4c7e8b07e66e6
Eric Busch <buscheric97@gmail.com>
2019-04-19 23:51:25 -0500

    Added comments as well as preprocessing in DetectWink2.py

--- DetectWink1.py
+++ DetectWink1.py
@@ -1,3 +1,12 @@
+# CS4391.001 - Project 2
+#
+# This program detects winking faces
+# either from a live video feed or
+# from a folder of images
+#
+# Program by: Eric Busch
+# edb160230@utdallas.edu
+
 import numpy as np
 import cv2
 import os
@@ -6,23 +15,25 @@ from os.path import isfile, join
 import sys
 
 def detectWink(frame, location, ROI, cascade):
+    # run cascade detection algorithm for eyes
     eyes = cascade.detectMultiScale(
         ROI, 1.15, 3, 0|cv2.CASCADE_SCALE_IMAGE, (10, 20))
+
+    # iterate through each eye detected in order to put a box around it
     for e in eyes:
         e[0] += location[0]
         e[1] += location[1]
         x, y, w, h = e[0], e[1], e[2], e[3]
 
-        cv2.rectangle(frame, (x,y), (x+w,y+h), (0, 0, 255), 2)
-    return len(eyes) == 1    # number of eyes is one
+        cv2.rectangle(frame, (x,y), (x+w,y+h), (0, 0, 255), 2) # put a red box around the detected eye
+
+    # return whether or not a winking face was detected (one eye is detected)
+    return len(eyes) == 1
 
 def detect(frame, faceCascade, eyesCascade):
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # convert the current frame to greyscale
 
-    # possible frame pre-processing:
-    # gray_frame = cv2.equalizeHist(gray_frame)
-    # gray_frame = cv2.medianBlur(gray_frame, 5)
-
+    # run cascade detection algorithm for faces
     scaleFactor = 1.15 # range is from 1 to ..
     minNeighbors = 3   # range is from 0 to ..
     flag = 0|cv2.CASCADE_SCALE_IMAGE # either 0 or 0|cv2.CASCADE_SCALE_IMAGE
@@ -34,15 +45,16 @@ def detect(frame, faceCascade, eyesCascade):
         flag,
         minSize)
 
+    # count the number of detected faces
     detected = 0
     for f in faces:
         x, y, w, h = f[0], f[1], f[2], f[3]
         faceROI = gray_frame[y:int(y+(h*0.6)), x:x+w] # Set faceROI to be the top 60% of the face because eyes are only ever in that area
         if detectWink(frame, (x, y), faceROI, eyesCascade):
-            detected += 1
-            cv2.rectangle(frame, (x,y), (x+w,y+h), (255, 0, 0), 2)
+            detected += 1 # increment detected if a winking face was detected
+            cv2.rectangle(frame, (x,y), (x+w,y+h), (255, 0, 0), 2) # put a blue box around the detected winking face
         else:
-            cv2.rectangle(frame, (x,y), (x+w,y+h), (0, 255, 0), 2)
+            cv2.rectangle(frame, (x,y), (x+w,y+h), (0, 255, 0), 2) # put a green box around the detected face
     return detected
 
 
@@ -51,12 +63,13 @@ def run_on_folder(cascade1, cascade2, folder):
         folder = folder + "/"
     files = [join(folder,f) for f in listdir(folder) if isfile(join(folder,f))]
 
+    # go through each image in the specified folder and detect faces
     windowName = None
     totalCount = 0
     for f in files:
         img = cv2.imread(f)
         if type(img) is np.ndarray:
-            lCnt = detect(img, cascade1, cascade2)
+            lCnt = detect(img, cascade1, cascade2) # run face detection algorithms
             totalCount += lCnt
             if windowName != None:
                 cv2.destroyWindow(windowName)
@@ -72,25 +85,26 @@ def runonVideo(face_cascade, eyes_cascade):
         print("Can't open default video camera!")
         exit()
 
+    # process live video feed for face detection
     windowName = "Live Video"
     showlive = True
     while(showlive):
-        ret, frame = videocapture.read()
+        ret, frame = videocapture.read() # get next frame
 
         if not ret:
             print("Can't capture frame")
             exit()
 
-        detect(frame, face_cascade, eyes_cascade)
+        detect(frame, face_cascade, eyes_cascade) # run face detection algorithms
         cv2.imshow(windowName, frame)
         if cv2.waitKey(30) >= 0:
             showlive = False
 
-    # outside the while loop
+    # release video capture
     videocapture.release()
     cv2.destroyAllWindows()
 
-
+# "main" routine
 if __name__ == "__main__":
     # check command line arguments: nothing or a folderpath
     if len(sys.argv) != 1 and len(sys.argv) != 2:
@@ -104,6 +118,7 @@ if __name__ == "__main__":
     eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades
                                       + 'haarcascade_eye.xml')
 
+    # determine whether to run on live video feed or folder of images
     if(len(sys.argv) == 2): # one argument
         folderName = sys.argv[1]
         detections = run_on_folder(face_cascade, eye_cascade, folderName)
